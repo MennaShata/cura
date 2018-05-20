375b9697232ccec5228b62c70f3a93e93a59426b
Asmaa Nour <asmaamo2so95@gmail.com>
2018-05-20 10:37:50 +0200

    create interface idisease

--- src/app/diseases/disease-item/disease-item.component.ts
+++ src/app/diseases/disease-item/disease-item.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Idisease } from '../data/interfaces/idisease';
+import { Idisease } from '../../shared/models/interfaces/idisease';
 
 @Component({
   selector: 'app-disease-component',
   templateUrl: './disease-component.component.html',
   styleUrls: ['./disease-component.component.css']
 })
-export class DiseaseComponentComponent implements OnInit {
+export class DiseaseItemComponent implements OnInit {
   @Input() disease:Idisease;
   constructor() { }
 item :string='clear';
