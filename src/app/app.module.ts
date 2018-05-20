3c2103526249dd285aceebe25c58e41eb8d8854b
SheroukSelim <sherouk.selim95@gmail.com>
2018-05-20 11:58:12 +0200

    pill 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PillListingComponent } from './pill/pill-listing/pill-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PillListingComponent
   ],
   imports: [
     BrowserModule
