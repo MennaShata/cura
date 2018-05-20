bdaa07c65bf5831296f37d84371dfe4f819e1167
SheroukSelim <sherouk.selim95@gmail.com>
2018-05-20 12:17:14 +0200

    el merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PillListingComponent } from './pill/pill-listing/pill-listing.component';
+import {RouterModule,Routes} from '@angular/router';
+import {DrugsModule} from './drugs/drugs.module';
+import {DrugsComponent} from './drugs/drugs.component';
+const routes: Routes = [
+  { path: '',component:AppComponent },
+  { path: 'drug/listing', component: DrugsComponent},
 
+];
 @NgModule({
   declarations: [
     AppComponent,
     PillListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DrugsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
