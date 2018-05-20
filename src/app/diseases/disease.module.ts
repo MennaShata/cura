22f02860d717442c8a0c988bc1ee98378f1221b2
Memadaldin <m.emadaldin9@gmail.com>
2018-05-20 13:36:52 +0200

    baby baby baby ooooo

--- src/app/diseases/disease.module.ts
+++ src/app/diseases/disease.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DiseaseAddComponent } from './disease-add/disease-add.component';
 import { DiseaseLisingComponent } from './disease-lising/disease-lising.component';
 import { DiseaseDetailsComponent } from './disease-details/disease-details.component';
-import { DiseaseItemComponent } from './disease-item/disease-item.component';
+import { DiseaseComponentComponent } from './disease-item/disease-item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DiseaseAddComponent, DiseaseLisingComponent, DiseaseDetailsComponent,
-    DiseaseItemComponent
+  declarations: [
+    DiseaseAddComponent, 
+    DiseaseLisingComponent, 
+    DiseaseDetailsComponent,
+    DiseaseComponentComponent,
 ]
 })
 export class DiseaseModule { }
