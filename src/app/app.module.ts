28e4c910cd875b0c368e2686e44e16a19303d570
MennaShata <Mennashata10@gmail.com>
2018-05-20 15:18:05 +0200

    menna 55

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import {RouterModule,Routes} from '@angular/router';
 import {DrugsModule} from './drugs/drugs.module';
 import {DrugsComponent} from './drugs/drugs.component';
 import { PillItemComponent } from './pill/pill-item/pill-item.component';
+import { SideEffectItemComponent } from './side-effects/side-effect-item/side-effect-item.component';
 const routes: Routes = [
   { path: '',component:AppComponent },
   { path: 'drug/listing', component: DrugsComponent},
   { path: 'food', component: FoodItemComponent}
+  {path :'sideeffect', component:SideEffectItemComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
     PillListingComponent,
     PillItemComponent,
-    FoodItemComponent
+    FoodItemComponent,
+    SideEffectItemComponent
+    
   ],
   imports: [
     BrowserModule,
