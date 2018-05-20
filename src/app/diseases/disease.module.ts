d4bc7660ddb09b1c5c3b6d1572c0b9470edc5db9
Asmaa Nour <asmaamo2so95@gmail.com>
2018-05-20 11:56:09 +0200

    disease changes

--- src/app/diseases/disease.module.ts
+++ src/app/diseases/disease.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DiseaseAddComponent } from './disease-add/disease-add.component';
 import { DiseaseLisingComponent } from './disease-lising/disease-lising.component';
 import { DiseaseDetailsComponent } from './disease-details/disease-details.component';
-import { DiseaseItemComponent } from './disease-item/disease-item.component';
+//import { DiseaseSmallItemComponent } from './diseases/disease-small-item/disease-small-item.component';
 import { DiseaseSmallItemComponent } from './disease-small-item/disease-small-item.component';
 
 @NgModule({
@@ -11,7 +11,6 @@ import { DiseaseSmallItemComponent } from './disease-small-item/disease-small-it
     CommonModule
   ],
   declarations: [DiseaseAddComponent, DiseaseLisingComponent, DiseaseDetailsComponent,
-    DiseaseItemComponent,
     DiseaseSmallItemComponent
 ]
 })
