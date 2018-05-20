2abfab2ba554dda04d095e889fb6a94481aa3ed3
ayaashraf <ayaashraf1111@gmIL.com>
2018-05-19 23:30:41 -0700

    drug-firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {RouterModule,Routes} from '@angular/router';
+import {DrugsModule} from './drugs/drugs.module';
+import {DrugsComponent} from './drugs/drugs.component';
+const routes: Routes = [
+  { path: '',component:AppComponent },
+  { path: 'drug/listing', component: DrugsComponent},
 
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DrugsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
