12ab2890c23ce595b992caafa1505638b4c66c0a
SheroukSelim <sherouk.selim95@gmail.com>
2018-05-21 14:41:50 +0200

    6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DiseaseSmallItemComponent } from './diseases/disease-small-item/disease-small-item.component';
-import { PillListingComponent } from './pill/pill-listing/pill-listing.component';
+
 import {RouterModule,Routes} from '@angular/router';
-import{PillModule} from './pill/pill.module';
+
 import {DrugsModule} from './drugs/drugs.module';
 import {DrugsComponent} from './drugs/drugs.component';
+
+import { PillModule } from './pill/pill.module';
+import { PillComponent } from './pill/pill.component';
 import { PillItemComponent } from './pill/pill-item/pill-item.component';
+import { PillListingComponent } from './pill/pill-listing/pill-listing.component';
+
 import { SideEffectItemComponent } from './side-effects/side-effect-item/side-effect-item.component';
 import { ActiveIngrediantItemComponent } from 'src/app/active-ingredients/active-ingrediant-item/active-ingrediant-item.component';
 import { ActiveIngredientListingComponent } from 'src/app/active-ingredients/active-ingredient-listing/active-ingredient-listing.component';
 import { FoodInteractionsListingComponent } from './Interactions/Food-Interaction/Food-Interactions-Listing/Food-Interactions-Listing.component';
+
 
 const routes: Routes = [
   { path: '',component:AppComponent },
@@ -23,10 +29,11 @@ const routes: Routes = [
   { path: 'food', component: FoodItemComponent},
   { path: 'food/add', component: FoodAddComponent},
   { path: 'interactions/foodinteraction', component: FoodInteractionItemComponent},
-  {path :'sideeffect', component:SideEffectItemComponent },
+  { path :'sideeffect', component:SideEffectItemComponent },
   { path:'ai', component: ActiveIngrediantItemComponent},
   { path:'food/foodlist', component: FoodListingComponent},
-  { path:'interactions/foodinteraction/listing', component: FoodInteractionsListingComponent}
+  { path:'interactions/foodinteraction/listing', component: FoodInteractionsListingComponent},
+  { path: 'pill/listing', component: PillComponent}
 ];
 @NgModule({
   declarations: [
