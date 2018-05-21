d434cb433fd722aeaaef5ddac55b6c9e1eff5a65
SheroukSelim <sherouk.selim95@gmail.com>
2018-05-21 14:05:36 +0200

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,29 @@ import { FoodItemComponent } from './food/food-item/food-item.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { PillListingComponent } from './pill/pill-listing/pill-listing.component';
 import {RouterModule,Routes} from '@angular/router';
+
 import {DrugsModule} from './drugs/drugs.module';
 import {DrugsComponent} from './drugs/drugs.component';
-import { PillItemComponent } from './pill/pill-item/pill-item.component';
+
+import { PillModule } from './pill/pill.module';
+import {PillComponent} from './pill/pill.component';
+
 const routes: Routes = [
   { path: '',component:AppComponent },
   { path: 'drug/listing', component: DrugsComponent},
+  { path: 'pill/item', component: PillComponent},
   { path: 'food', component: FoodItemComponent}
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    PillListingComponent,
-    PillItemComponent,
     FoodItemComponent
   ],
   imports: [
     BrowserModule,
     DrugsModule,
+    PillModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
