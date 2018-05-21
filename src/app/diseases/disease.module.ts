8044a204dea70dd52c5614e173a3da5f74838bc2
Asmaa Nour <asmaamo2so95@gmail.com>
2018-05-21 10:46:05 +0200

    disease change2

--- src/app/diseases/disease.module.ts
+++ src/app/diseases/disease.module.ts
@@ -4,6 +4,8 @@ import { DiseaseAddComponent } from './disease-add/disease-add.component';
 import { DiseaseLisingComponent } from './disease-lising/disease-lising.component';
 import { DiseaseDetailsComponent } from './disease-details/disease-details.component';
 import { DiseaseComponentComponent } from './disease-component/disease-component.component';
+import { FilterComponent } from '../shared/filter/filter.component';
+import { DiseaseSmallItemComponent } from './disease-small-item/disease-small-item.component';
 
 @NgModule({
   imports: [
@@ -12,7 +14,10 @@ import { DiseaseComponentComponent } from './disease-component/disease-component
   declarations: [
     DiseaseAddComponent, 
     DiseaseLisingComponent, 
-    DiseaseDetailsComponent,,
+    DiseaseDetailsComponent,
+    FilterComponent,
+    DiseaseSmallItemComponent,
+,
     DiseaseComponentComponent
 ]
 })
