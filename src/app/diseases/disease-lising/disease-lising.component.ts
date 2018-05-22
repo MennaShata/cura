a6c899313cb4bb9f38662c850f9799cdd6ca069d
Asmaa Nour <asmaamo2so95@gmail.com>
2018-05-22 10:50:32 +0200

    disease changes 22/5

--- src/app/diseases/disease-lising/disease-lising.component.ts
+++ src/app/diseases/disease-lising/disease-lising.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DiseaseSmallItemComponent } from '../disease-small-item/disease-small-item.component';
+import { Idisease } from '../../shared/models/interfaces/idisease';
+import { DiseaseServiceService } from '../../shared/services/disease-service.service';
 
 @Component({
   selector: 'app-disease-lising',
@@ -7,10 +9,13 @@ import { DiseaseSmallItemComponent } from '../disease-small-item/disease-small-i
   styleUrls: ['./disease-lising.component.css']
 })
 export class DiseaseLisingComponent implements OnInit {
-
-  constructor() { }
+ diseases:Idisease[];
+  constructor( private diseaseServiceService:DiseaseServiceService) { }
+public getDiseases(){
+  this.diseases=this.diseaseServiceService.getDiseases();
+}
   ngOnInit() {
-    
+    this.getDiseases();
   }
 
 }
