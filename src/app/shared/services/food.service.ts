9d9234b453ded03e8ed72cfd17aef849118000cf
Memadaldin <m.emadaldin9@gmail.com>
2018-05-31 13:08:33 +0200

    food service

--- src/app/shared/services/food.service.ts
+++ src/app/shared/services/food.service.ts
@@ -21,9 +21,21 @@ export class FoodService {
       this.foodArray.splice(index,1)
     }
   }
+  public getById(id:number){
+    const index = this.foodArray.findIndex(a=>a.id ==id);
+    if(index >-1){
+      return this.foodArray[index];
+    }
+
+  }
   public add(food:Ifood){
     this.foodArray.push(food);
 
+  }
+  public save(food:Ifood){
+    const oldFood = this.getById(food.id);
+    oldFood.icon = food.icon;
+    oldFood.name = food.name
   }
 constructor() { }
 
