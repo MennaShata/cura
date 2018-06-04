26446413da28307de3ec8195bda886636391d4b0
MennaShata <Mennashata10@gmail.com>
2018-06-04 13:57:29 +0200

    el search ya emad wshwit ashia2 fel side effect add

--- src/app/shared/models/interfaces/Isideeffect.ts
+++ src/app/shared/models/interfaces/Isideeffect.ts
@@ -2,6 +2,5 @@ export interface Isideeffect {
     id?:number;
     name:string;
     description:string;
-    icon:string;
     isDeleted?:boolean;
 }
--- src/app/side-effects/side-effects.component.ts
+++ src/app/side-effects/side-effects.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { SideEffectAddComponent } from './side-effect-add/side-effect-add.component';
 
 @Component({
   selector: 'app-side-effects',
@@ -6,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-effects.component.css']
 })
 export class SideEffectsComponent implements OnInit {
-
-  constructor() { }
-
+  bsModalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { }
+  public openModalWithComponent() {
+    this.bsModalRef = this.modalService.show(SideEffectAddComponent);
+  }
   ngOnInit() {
   }
 
