3083578bf51e8716a53218c96c4f9e65bf4c1500
ayaashraf <ayaashraf1111@gmIL.com>
2018-06-05 14:49:51 -0700

    interaction

---
+++ src/app/Interactions/drug-interaction/drug-interaction-item/drug-interaction-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drug-interaction-item',
+  templateUrl: './drug-interaction-item.component.html',
+  styleUrls: ['./drug-interaction-item.component.css']
+})
+export class DrugInteractionItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
