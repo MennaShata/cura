cd98e3ec597f1285dd134e441b230ad89a4081fe
MennaShata <Mennashata10@gmail.com>
2018-06-05 14:46:32 +0200

    add form with validation

--- src/app/side-effects/side-effect-add/side-effect-add.component.ts
+++ src/app/side-effects/side-effect-add/side-effect-add.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {NgControl} from '@angular/forms';
+import { NgControl } from '@angular/forms';
 import { Isideeffect } from '../../shared/models/interfaces/Isideeffect';
 import { SideeffectService } from '../../shared/services/side-effect/sideeffect.service';
 import { BsModalRef } from 'ngx-bootstrap';
@@ -12,32 +12,33 @@ import { BsModalRef } from 'ngx-bootstrap';
   styleUrls: ['./side-effect-add.component.css']
 })
 export class SideEffectAddComponent implements OnInit {
-sideeffect:Isideeffect;
-myForm:FormGroup;
-flag = true;
-  constructor(private sideeffectService:SideeffectService,public bsModalRef: BsModalRef) { }
+  sideeffect: Isideeffect;
+  myForm: FormGroup;
+  flag: boolean;
+  constructor(private sideeffectService: SideeffectService, public bsModalRef: BsModalRef) { }
 
   ngOnInit() {
-    this.myForm=new FormGroup({
-      name:new FormControl('',Validators.required),
-      description:new FormControl('',Validators.required),
-     
-      
+    this.myForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+
+
     });
   }
-public addsideeffect(){
-  
-  if(this.myForm.get('name').value!=''&&this.myForm.get('description').value!='')
-  {
-  this.sideeffect={
-    name:this.myForm.get('name').value,
-    description:this.myForm.get('description').value,
-    
+  public addsideeffect() {
+
+    if (this.myForm.get('name').value != '' && this.myForm.get('description').value != '') {
+      this.flag = true;
+      this.sideeffect = {
+        name: this.myForm.get('name').value,
+        description: this.myForm.get('description').value,
+
+      }
+      this.sideeffectService.add(this.sideeffect)
+      this.myForm.reset();
+    }
+    else {
+      this.flag = false
+    }
   }
-  this.sideeffectService.add(this.sideeffect)
-}
-else{
-this.flag = false
-}
-}
 }
