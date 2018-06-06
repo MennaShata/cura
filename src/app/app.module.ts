2753b8935f3a5aaab26e92b7744100630344ebed
SheroukSelim <sherouk.selim95@gmail.com>
2018-06-06 17:13:12 +0200

    apill edit and item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { PillComponent } from './pill/pill.component';
 import { PillItemComponent } from './pill/pill-item/pill-item.component';
 
 import { SideEffectItemComponent } from './side-effects/side-effect-item/side-effect-item.component';
-import { FormsModule ,ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule ,ReactiveFormsModule, FormGroup, FormControl }   from '@angular/forms';
 import { DiseaseSmallItemComponent } from './diseases/disease-small-item/disease-small-item.component';
 import { DiseaseLisingComponent } from './diseases/disease-lising/disease-lising.component';
 import { DiseaseComponentComponent } from './diseases/disease-component/disease-component.component';
@@ -146,7 +146,8 @@ const routes: Routes = [
     SharedModule,
     [ReactiveFormsModule],
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+  
   ],
   providers: [
     DiseaseServiceService,
