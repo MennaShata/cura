5dcbfd8b940d4d3d67292494d848d7721d08ead9
SheroukSelim <sherouk.selim95@gmail.com>
2018-06-10 03:45:40 +0200

    edit and search

--- src/app/shared/services/color/color.service.ts
+++ src/app/shared/services/color/color.service.ts
@@ -5,45 +5,45 @@ import { Icolor } from '../../models/interfaces/Icolor';
   providedIn: 'root'
 })
 export class ColorService {
-private colors : Icolor[];
-private color:Icolor;
+  private colors: Icolor[];
+  private color: Icolor;
 
-constructor() {
-  this.colors=[{id:0,name:'White'},
-  {id:1,name:'Off White'},
-  {id:2,name:'Orange'},
-  {id:3,name:'Yellow'},
-  {id:4,name:'Gray'},
-  {id:5,name:'Green'},
-  {id:6,name:'Tan'},
-  {id:7,name:'Brown'},
-  {id:8,name:'Red'},
-  {id:9,name:'Pink'},
-  {id:10,name:'Peach'},
-  {id:11,name:'Gold'},
-  {id:12,name:'Blue'},
-  {id:13,name:'Purple'},
-  {id:14,name:'Lavander'},
-  {id:5,name:'Multi Color'}];
- }
- public getAll():Icolor[]
- {
-  return this.colors
- }
- public getName(id:number):string{
-  const index = this.colors.findIndex(a=>a.id ==id);
-  if(index >-1){
-    return this.colors[index].name;
+  constructor() {
+    this.colors = [
+      { id: 1, name: 'White' },
+      { id: 2, name: 'Orange' },
+      { id: 3, name: 'Yellow' },
+      { id: 4, name: 'Gray' },
+      { id: 5, name: 'Green' },
+      { id: 6, name: 'Tan' },
+      { id: 7, name: 'Brown' },
+      { id: 8, name: 'Red' },
+      { id: 9, name: 'Pink' },
+      { id: 10, name: 'Peach' },
+      { id: 11, name: 'Gold' },
+      { id: 12, name: 'Blue' },
+      { id: 13, name: 'Purple' },
+      { id: 14, name: 'Lavander' },
+      { id: 15, name: 'Multi Color' },
+      { id: 16, name: 'Off White' }];
   }
-}
-
-getById(id) {
-  for (let i = 0; i < this.colors.length; i++) {
-    if (this.colors[i].id == id) {
-      this.color= this.colors[i];
+  public getAll(): Icolor[] {
+    return this.colors
+  }
+  public getName(id: number): string {
+    const index = this.colors.findIndex(a => a.id == id);
+    if (index > -1) {
+      return this.colors[index].name;
     }
   }
-  return this.color;
-}
+
+  getById(id) {
+    for (let i = 0; i < this.colors.length; i++) {
+      if (this.colors[i].id == id) {
+        this.color = this.colors[i];
+      }
+    }
+    return this.color;
+  }
 
 }
