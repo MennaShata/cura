1e1e8e0ffbb57da40708d3c225fb267ec4601f24
Memadaldin <m.emadaldin9@gmail.com>
2018-06-10 12:56:28 +0200

    اوعى وشك و القطر

--- src/app/food/food-edit/food-edit.component.ts
+++ src/app/food/food-edit/food-edit.component.ts
@@ -3,6 +3,8 @@ import { FoodService } from './../../shared/services/food.service';
 import { Ifood } from './../../shared/models/interfaces/ifood';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service'
 
 @Component({
   selector: 'app-food-edit',
