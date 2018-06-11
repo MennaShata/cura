4f08174dcd815d6cdf5930ae10532759f8194afb
MennaShata <Mennashata10@gmail.com>
2018-06-11 12:33:35 +0200

    user

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   declarations: [UserComponent,
     LoginComponent,
