0109842729afbcfb5e7f9f369a2263cd487d12df
ayaashraf <ayaashraf1111@gmIL.com>
2018-06-11 22:31:53 -0700

    homepage2

--- src/app/shared/models/interfaces/Iuser.ts
+++ src/app/shared/models/interfaces/Iuser.ts
@@ -2,14 +2,14 @@ export interface Iuser {
     username:string
     email:string
     password:string
-   firstName: string;
-   lastName: string;
-   profession: string;
-   fieldOfSpeciality: string;
-   phoneNumber: number;
-   dateOfBirth: any;
-   image: string;
-   country: string;
-   city: string;
+   firstName?: string;
+   lastName?: string;
+   profession?: string;
+   fieldOfSpeciality?: string;
+   phoneNumber?: number;
+   dateOfBirth?: any;
+   image?: string;
+   country?: string;
+   city?: string;
 
 }
