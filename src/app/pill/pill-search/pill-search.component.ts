cf51ffc98029ee48b3d83a5c30c9547e9a509662
SheroukSelim <sherouk.selim95@gmail.com>
2018-06-17 21:40:29 +0200

    awl edit anhrda  fl update

--- src/app/pill/pill-search/pill-search.component.ts
+++ src/app/pill/pill-search/pill-search.component.ts
@@ -3,7 +3,7 @@ import { PillService } from '../../shared/services/pill/Pill.service';
 import { ColorService } from '../../shared/services/color/color.service';
 import { ShapeService } from '../../shared/services/shape/shape.service';
 import { FormControl } from '@angular/forms';
-import { FormGroup,  Validators } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 import { Ishape } from '../../shared/models/interfaces/ishape';
 import { Icolor } from '../../shared/models/interfaces/Icolor';
 import { Ipill } from '../../shared/models/interfaces/ipill';
@@ -15,21 +15,21 @@ import { HelperFunctions } from '../../shared/classes/helper';
   styleUrls: ['./pill-search.component.css']
 })
 export class PillSearchComponent implements OnInit {
-  isDisabled= true;
+  isDisabled = true;
   color: any;
   shape: any;
-  backImprint:string;
+  backImprint: string;
   frontImprint: string;
   searchForm: FormGroup;
   shapes: Ishape[];
   colors: Icolor[];
   pill: any;
-  // public menuMessage: any ;
+
 
   constructor(private pillService: PillService, private colorService: ColorService, private shapeService: ShapeService) { }
 
   ngOnInit() {
-    // this.menuMessage = "countryCodeAFcountryNameAfghanistan";
+
     this.colors = this.colorService.getAll();
     this.shapes = this.shapeService.getAll();
 
@@ -40,30 +40,23 @@ export class PillSearchComponent implements OnInit {
       Color: new FormControl(),
     });
 
- 
-
   }
-   
-  onDataChanged()
-  {
+
+  onDataChanged() {
     this.frontImprint = this.searchForm.get('FrontImprint').value;
     this.backImprint = this.searchForm.get('BackImprint').value;
     this.shape = this.searchForm.get('Shape').value;
     this.color = this.searchForm.get('Color').value;
 
-    if(!HelperFunctions.isNullOrEmpty(this.frontImprint) || !HelperFunctions.isNullOrEmpty(this.backImprint) || (!HelperFunctions.isNullOrEmpty(this.color) && this.color != 0) || (!HelperFunctions.isNullOrEmpty(this.shape) && this.shape != 0))
-    {  
-      this.isDisabled  = false  ;
+    if (!HelperFunctions.isNullOrEmpty(this.frontImprint) || !HelperFunctions.isNullOrEmpty(this.backImprint) || (!HelperFunctions.isNullOrEmpty(this.color) && this.color != 0) || (!HelperFunctions.isNullOrEmpty(this.shape) && this.shape != 0)) {
+      this.isDisabled = false;
     }
-    else
-    {
-      this.isDisabled  = true  ;
+    else {
+      this.isDisabled = true;
     }
 
   }
 
-
-  
   searchPill() {
 
     this.frontImprint = this.searchForm.get('FrontImprint').value;
@@ -71,15 +64,15 @@ export class PillSearchComponent implements OnInit {
     this.shape = this.searchForm.get('Shape').value;
     this.color = this.searchForm.get('Color').value;
 
-      let pill:Ipill = { 
-        frontImprint : this.frontImprint , 
-        backImprint : this.backImprint, 
-        shape: this.shape,
-        color: this.color
-       };
-       this.pillService.onSerch.next(pill);
-       window.scrollTo(1000,1000)
-      //this.pillService.Search(this.frontImprint,this.backImprint,this.shape,this.color);
+    let pill: Ipill = {
+      frontImprint: this.frontImprint,
+      backImprint: this.backImprint,
+      shape: this.shape,
+      color: this.color
+    };
+    this.pillService.onSerch.next(pill);
+    window.scrollTo(1000, 1000)
+    //this.pillService.Search(this.frontImprint,this.backImprint,this.shape,this.color);
   }
 
 }
