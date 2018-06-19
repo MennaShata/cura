0495cc3417797899795fd56e8f47a9ef447f1227
ayaashraf <ayaashraf1111@gmIL.com>
2018-06-19 10:58:33 -0700

    homepage

--- src/app/shared/services/service/service.service.ts
+++ src/app/shared/services/service/service.service.ts
@@ -7,26 +7,32 @@ import { Iservice } from '../../models/interfaces/Iservice';
 export class ServiceService {
   private serviceArray:Iservice[]=[
     {
+      id:1,
       title:'Pill Identifier',
       description:'Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.'
    },
    {
+     id:2,
      title:'Symptom Checker',
      description:'Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.'
    },
    {
+     id:3,
      title:'Interaction Checker',
      description:'Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.',
    },
    {
+     id:4,
      title:'Ask a question!',
      description:'Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.',
    },
    {
+     id:5,
      title:'Meet health Professionals',
      description:'Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.',
    },
    {
+     id:6,
      title:'Add Your Medicine List',
      description:'Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.',
    }
